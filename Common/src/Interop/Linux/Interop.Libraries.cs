1d7c4625f1835bc05000f8687105b4b102daca6d
Kitlith <kitlith@kitl.pw>
2025-09-27 19:41:55 -0700

    Drop the last of the (linux) non-msquic interop

--- Common/src/Interop/Linux/Interop.Libraries.cs
+++ Common/src/Interop/Linux/Interop.Libraries.cs
@@ -5,12 +5,6 @@ internal static partial class Interop
 {
     internal static partial class Libraries
     {
-        // Duplicated from Android for Linux Bionic
-        internal const string Liblog = "liblog";
-
-        // As a convention, some ldap installs use this to reference the latest version.
-        internal const string OpenLdap = "libldap.so.2";
-
         internal const string MsQuic = "libmsquic.so";
     }
 }
